d2069147dc7a57af2786b1711be2904c0fb9ccc1
mac <mac@apps-MacBook-Air.local>
2025-07-01 13:55:56 +0500

    Initial Commit-QuickStore

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+    plugin(function ({ addVariant }) {
+      addVariant('active-link', '&.active-link'),
+      addVariant('group-active-link', ':merge(.group).active-link &'),
+      addVariant('hocus', ['&:hover', '&:focus'])
+    })
+  ],
+  content: [
+    './app/views/**/*.html.erb',
+    './app/views/**/*.turbo_stream.erb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
+  ],
+  theme: {
+
+  }
+}
