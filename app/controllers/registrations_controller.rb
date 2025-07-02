ba8cb18ee3484d675252c99a7aa86bb478b75222
Areeba Javed <areebajaved78@gmail.com>
2025-07-02 13:28:56 +0500

    Made changes in the from field

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
-
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
   end
 end
